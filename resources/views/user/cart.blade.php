b7b081a9200142b60a103765ef6b9b2ac3712504
Atayağız Uslu <57071570+Atayazz@users.noreply.github.com>
2023-05-07 19:44:39 +0300

    a
    a

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -1,9 +1,12 @@
 <!doctype html>
 <html lang="tr">
+
 <head>
     @include('user.css')
 </head>
+
 <body>
+
     <div class="container-fluid">
         <div class="row">
             <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,36 +14,72 @@
             </button>
             <div class="col-md-8 ms-sm-auto col-lg-9 p-0">
                 @include('user.sidebar')
-                @if(session()->has('message'))
-                <div class="alert alert-success">
-                    <button type="button" class="close" data-dismiss="alert">x</button>
-                    {{session()->get('message')}}
-                </div>
-                @endif
-                <table align="center">
-                    <tr>
-                        <td>Title</td>
-                        <td>Quantity</td>
-                        <td>Price</td>
-                        <td>Action</td>
-                    </tr>
-                    <form action="{{url('order')}}" method:"POST">
-                        @foreach($cart as $data)
-                        <tr>
-                            <td> <input type="text" name="title[]" value="{{$data->title}}" hidden />{{$data->title}}</td>
-                            <td><input type="text" name="quantity[]" value="{{$data->quantity}}" hidden />{{$data->quantity}}</td>
-                            <td><input type="text" name="price[]" value="{{$data->price}}" hidden />{{$data->price}}</td>
-                            <td><a class="btn btn-danger" href="{{url('delete',$data->id)}}">Delete</a></td>
-                        </tr>
-                        @endforeach
-                </table>
 
-                <button class="btn btn-success">Confirm Order</button>
-                </form>
+
+                <div class="container px-3 my-5 clearfix">
+    <!-- Shopping cart table -->
+    <div class="card">
+        <div class="card-header">
+            <h2>Shopping Cart</h2>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+              <table class="table table-bordered m-0">
+                <thead>
+                  <tr>
+                    <!-- Set columns width -->
+                    <th class="text-center py-3 px-4" style="min-width: 400px;">Product Name &amp; Details</th>
+                    <th class="text-right py-3 px-4" style="width: 100px;">Price</th>
+                    <th class="text-center py-3 px-4" style="width: 120px;">Quantity</th>
+                    <th class="text-right py-3 px-4" style="width: 100px;">Total</th>
+                    <th class="text-center align-middle py-3 px-0" style="width: 40px;"><a href="#" class="shop-tooltip float-none text-light" title="" data-original-title="Clear cart"><i class="ino ion-md-trash"></i></a></th>
+                  </tr>
+                </thead>
+                <tbody>
+        
+                  <tr>
+                    <td class="p-4">
+                      <div class="media align-items-center">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="d-block ui-w-40 ui-bordered mr-4" alt="">
+                        <div class="media-body">
+                          <a href="#" class="d-block text-dark">Product 1</a>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="text-right font-weight-semibold align-middle p-4">$57.55</td>
+                    <td class="align-middle p-4"><input type="text" class="form-control text-center" value="2"></td>
+                    <td class="text-right font-weight-semibold align-middle p-4">$115.1</td>
+                    <td class="text-center align-middle px-0"><a href="#" class="shop-tooltip close float-none text-danger" title="" data-original-title="Remove">×</a></td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
+            <!-- / Shopping cart table -->
+        
+            <div class="d-flex flex-wrap justify-content-between align-items-center pb-4">
+              <div class="d-flex">
+                <div class="text-right mt-4 mr-5">
+                  <label class="text-muted font-weight-normal m-0">Discount</label>
+                  <div class="text-large"><strong>$20</strong></div>
+                </div>
+                <div class="text-right mt-4">
+                  <label class="text-muted font-weight-normal m-0">Total price</label>
+                  <div class="text-large"><strong>$1164.65</strong></div>
+                </div>
+              </div>
+            </div>
+        
+            <div class="float-right">
+              <button style="color:blue !important;" type="button" class="btn btn-primary mt-2">Checkout</button>
+            </div>
+          </div>
+      </div>
+  </div>
+
             @include('user.footer')
 
         </div>
         @include('user.js')
 </body>
+
 </html>
